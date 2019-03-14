04c29e14357aef5fefc1eb9bd902bd820a2814c3
Maggie Carpenter <carpmh17@mit.edu>
2019-03-14 08:05:50 -0400

    Initial Commit

---
+++ Bear App 3Tests/Bear_App_3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Bear_App_3Tests.swift
+//  Bear App 3Tests
+//
+//  Created by Margaret Carpenter on 3/14/19.
+//  Copyright © 2019 Margaret Carpenter. All rights reserved.
+//
+
+import XCTest
+@testable import Bear_App_3
+
+class Bear_App_3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
