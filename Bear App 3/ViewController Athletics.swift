b92d6e2cdf931f3fc4ab140c079dbad85eb668e3
Maggie Carpenter <carpmh17@mit.edu>
2019-03-27 19:41:45 -0400

    March 27 2019

---
+++ Bear App 3/ViewController Athletics.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController Athletics.swift
+//  Bear App 3
+//
+//  Created by Margaret Carpenter on 3/27/19.
+//  Copyright © 2019 Margaret Carpenter. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+
+class ViewController_Athletics: UIViewController , WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    
+    override func viewDidLoad() {
+        
+        let url = URL(string: "http://www.chittenangoschools.org/teacherpage.cfm?teacher=1677")!
+        webView.load(URLRequest(url: url))
+        
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refresh]
+        
+        navigationController?.isToolbarHidden = false
+        
+        //super.viewDidLoad()
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!)
+    {title = webView.title}
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
---
+++ Bear App 3/ViewController Youtube Page.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController Youtube Page.swift
+//  Bear App 3
+//
+//  Created by Margaret Carpenter on 3/27/19.
+//  Copyright © 2019 Margaret Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController_Youtube_Page: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
